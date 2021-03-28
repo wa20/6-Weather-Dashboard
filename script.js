e2301c31e723f9f0c36cd6052aa31d26c206d5b1
wael ahmed <w.ahmed020@gmail.com>
2021-03-28 12:17:45 +0100

    code updates

--- script.js
+++ script.js
@@ -2,10 +2,28 @@
 
 // time and date elements
 var time = moment().format("HH:mm A");
-$(".time").text(time);
+$("#time").text(time);
 
 var day = moment().format("dddd");
-$(".today").text(day);
+$("#today").text(day);
 
 var date = moment().format("Do MMMM YYYY");
 $(".date").text(date);
+
+
+
+//temp cards date
+var dayOne = moment().format("ddd");
+$('#day1').text(dayOne);
+
+var dayTwo = moment().add(1, 'days').format("ddd");
+$('#day2').text(dayTwo);
+
+var dayThree = moment().add(2, 'days').format("ddd");
+$('#day3').text(dayThree);
+
+var dayFour = moment().add(3, 'days').format("ddd");
+$('#day4').text(dayFour);
+
+var dayFive = moment().add(4, 'days').format("ddd");
+$('#day5').text(dayFive);
