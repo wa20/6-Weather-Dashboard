78740cddaec1141b0f4d0a4e1b3e31ff9049a805
wael ahmed <w.ahmed020@gmail.com>
2021-03-29 11:10:26 +0100

    code updates

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ $("#day6").text(daySix);
 
 //search weather
 
+
+
 function searchWeather(cityName) {
 
   var location = document.querySelector("#search").value;
@@ -53,38 +55,10 @@ function searchWeather(cityName) {
 }
 
 
-//celsus vs fahrenheit
-const tempC = document.getElementById("tempC");
-const tempF = document.getElementById("tempF")
-
-var unit = 'metric';
-
-
-tempC.addEventListener("click", function (unit) {
-    if (unit === "metric") {
-      unit = "imperial";
-    } else {
-      mode = "imperial";
-    
-    }
-  });
-
-  tempF.addEventListener("click", function (unit) {
-    if (unit === "metric") {
-      unit = "imperial";
-    } else {
-      mode = "imperial";
-    
-    }
-  });
-
-
-
-
 
 function oneCallApi(lat, long) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&units=${unit}&appid=${APIKEY}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&units=metric&appid=${APIKEY}`
   )
     .then(function (response) {
       return response.json();
@@ -94,20 +68,53 @@ function oneCallApi(lat, long) {
       console.log(data.daily[0]);
 
       // weather temp today
-      document.querySelector("#currentTempDay").textContent = data.current.temp + " °";
-      document.querySelector("#feelsLikeDay").textContent = data.current.feels_like + " °";
+      document.querySelector("#currentTempDay").textContent = data.current.temp + "°";
+      document.querySelector("#feelsLikeDay").textContent = data.current.feels_like + "°";
       document.querySelector("#description").textContent = data.current.weather[0].description;
+
+
+      // weather highlights today
+    
+    // sunrise and sunset
+     var sRise = data.daily[0].sunrise
+     var sunriseConverted = new Date(sRise * 1000).toLocaleTimeString([], {timeStyle: 'short'});
+     console.log(sunriseConverted);
+
+     var sSet = data.daily[0].sunset
+     var sunsetConverted = new Date(sSet,).toLocaleTimeString([], {timeStyle: 'short'});
+     console.log(sunsetConverted);
+
+     var timeZone = data.timezone;
+
+
+     document.querySelector("#timezone").textContent = "Timezone: " + timeZone
+     document.querySelector("#sunrise").textContent = "Sunrise: " + sunriseConverted
+     document.querySelector("#sunset").textContent = "Sunset: " + sunsetConverted
+     
+
+     // sunrise and sunset
+
+
+     document.querySelector("#dayTemp").textContent = "Day: " + data.daily[0].temp.day + "°";
+
+     document.querySelector("#nightTemp").textContent = "Night: " + data.daily[0].temp.night + "°";
+
+     document.querySelector("#humidity").textContent = "humidity: " + data.daily[0].humidity +"%";
+
+
+
+
 
     //   document.querySelector("#currentTempEve").textContent = data.daily[0].temp.night;
     //   document.querySelector("#feelsLikeEve").textContent = data.daily[0].feels_like.night;
 
      // daily temp rest of week days
-      document.querySelector("#one .temp").textContent = data.daily[1].temp.day + " °";
-      document.querySelector("#two .temp").textContent = data.daily[2].temp.day + " °";
-      document.querySelector("#three .temp").textContent = data.daily[3].temp.day + " °";
-      document.querySelector("#four .temp").textContent = data.daily[4].temp.day + " °";
-      document.querySelector("#five .temp").textContent = data.daily[5].temp.day + " °";
-      document.querySelector("#six .temp").textContent = data.daily[6].temp.day + " °";
+      document.querySelector("#one .temp").textContent = data.daily[1].temp.day + "°";
+      document.querySelector("#two .temp").textContent = data.daily[2].temp.day + "°";
+      document.querySelector("#three .temp").textContent = data.daily[3].temp.day + "°";
+      document.querySelector("#four .temp").textContent = data.daily[4].temp.day + "°";
+      document.querySelector("#five .temp").textContent = data.daily[5].temp.day + "°";
+      document.querySelector("#six .temp").textContent = data.daily[6].temp.day + "°";
 
 
       // weather icon today
@@ -189,3 +196,30 @@ defaultCity ()
 
 document.querySelector(".searchButton").addEventListener("click", searchWeather);
 
+
+
+//celsus vs fahrenheit
+// const tempC = document.getElementById("tempC");
+// const tempF = document.getElementById("tempF")
+
+// var unit = 'metric';
+
+
+// tempC.addEventListener("click", function (unit) {
+
+//     if (unit === "metric") {
+//       unit = "imperial";
+//     } else {
+//       unit = "imperial";
+    
+//     }
+//   });
+
+//   tempF.addEventListener("click", function (unit) {
+//     if (unit === "metric") {
+//       unit = "imperial";
+//     } else {
+//       unit = "imperial";
+    
+//     }
+//   });
